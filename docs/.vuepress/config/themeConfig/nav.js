256a42c5a0ed84db189ed060fe542aaf2a69cc45
yexianchao <yxc123456>
2022-07-27 18:34:29 +0800

    导航

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -1,9 +1,9 @@
 // nav
 module.exports = [
   { text: '首页', link: '/' },
-  { text: '编程',link: '/one/',},
-  { text: '运维',link: '/two/',},
-  { text: '总结', link: '/three/' },
+  { text: '文章',link: '/文章/',},
+  { text: '后端',link: '/后端/',},
+  { text: '前端', link: '/前端/' },
   { text: '留言板', link: '/message-board/' },
   { text: '我的博客', link: 'https://www.zeroye.site' },
 ]
