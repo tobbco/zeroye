0fae6dda5e8aca724701c1fd23c02d1eb36329f4
yexianchao <yxc123456>
2022-10-13 09:57:41 +0800

    init

--- docs/.vuepress/config/themeConfig/nav.js
+++
@@ -1,9 +0,0 @@
-// nav
-module.exports = [
-  { text: '首页', link: '/' },
-  { text: '文章',link: '/文章/',},
-  { text: '后端',link: '/后端/',},
-  { text: '前端', link: '/前端/' },
-  { text: '留言板', link: '/留言板/' },
-  { text: '我的博客', link: 'https://www.zeroye.site' },
-]
